9fde95b1ae8154c00c03a42c3e8f699c48f0468e
RionNg <147371006+RionNg@users.noreply.github.com>
2023-11-04 18:34:11 +0800

    PaymentContract.sol

--- PaymentContract.sol
+++ PaymentContract.sol
@@ -3,12 +3,12 @@
 pragma solidity 0.8.22;
 
 contract PaymentContract {
-    event Payment(address indexed _from, uint _value, uint _refund);
+    event Payment(address indexed _from, uint256 _value, uint256 _refund);
 
-    function processPayment(uint _number) external payable {
+    function processPayment(uint256 _number) external payable {
         require(_number != 9, "We don't like 9");
         if (_number % 2 == 0) {
-            uint refundAmount = msg.value /2;
+            uint256 refundAmount = msg.value /2;
             payable (msg.sender).transfer(refundAmount);
             emit Payment(msg.sender, msg.value, refundAmount);
         } else {
